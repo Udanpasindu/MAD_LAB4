8994781d9b86e84a6f232b71df6d437eca418335
Udan Pasindu Jayasekara <151393273+Udanpasindu@users.noreply.github.com>
2024-05-11 18:11:18 +0530

    create NotesAdapter.kt file

---
+++ app/src/main/java/com/example/notessqlite/NotesAdapter.kt
@@ -0,0 +1,9 @@
+package com.example.notessqlite
+
+import android.content.Context
+import androidx.recyclerview.widget.RecyclerView
+
+class NotesAdapter(private var notes: List<Note>,context: Context):RecyclerView.Adapter<NotesAdapter.NoteViewHolder> {
+
+    class NoteViewHolder
+}
